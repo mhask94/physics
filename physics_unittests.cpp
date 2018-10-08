9cb7c81d30602696e31c39b0d5acae088e8b9e34
Mat Haskell <mhaskell9@gmail.com>
2018-10-08 15:24:30 -0600

    Physics Lib functional for assignment

--- physics_unittests.cpp
+++ physics_unittests.cpp
@@ -5,8 +5,7 @@
 
 bool vec3ExpectNear(phys::vec3 a,phys::vec3 b,float delta)
 {
-    phys::vec3 c = a - b;
-    c = phys::vec3::abs(abs(c);
+    phys::vec3 c = phys::vec3::abs(a - b);
     return(c.getX()<delta && c.getY()<delta && c.getZ()<delta);
 }
 
@@ -107,18 +106,35 @@ TEST(WorldWithNoGravityOrDensity,AskedToUpdate_UpdatesAllShperesCorrectly)
 
     phys::vec3 expected_pos1{0.1,-0.1,0};
     phys::vec3 expected_pos2{5,4,6};
-//    float dt{world.getDt()};
-//    phys::vec3 expected_pos1{0,0,0};
-//    phys::vec3 expected_vel1{0,0,0};
-//    phys::vec3 expected_pos2{0,0,0};
-//    phys::vec3 expected_vel2{0,0,0};
-//    expected_vel1 = world.getGravity()*dt;
-//    expected_pos1 = init_pos1 + expected_vel1*dt;
-//    expected_vel2 = world.getGravity()*dt;
-//    expected_pos2 = init_pos2 + expected_vel2*dt;
 
-    phys::vec3 test1 = sphere1->getPosition();
-    phys::vec3 test2 = sphere2->getPosition();
+    EXPECT_TRUE(vec3ExpectNear(expected_pos1,sphere1->getPosition(),.001f));
+    EXPECT_TRUE(vec3ExpectNear(expected_pos2,sphere2->getPosition(),.001f));
+}
+
+TEST(WorldWithNoDensity,AskedToUpdate_UpdatesAllShperesCorrectly)
+{
+    float mass{1};
+    phys::vec3 init_pos1{0,0,0};
+    phys::vec3 init_vel1{0,0,0};
+    phys::Sphere *sphere1{nullptr};
+    sphere1 = new phys::Sphere{mass,init_pos1,init_vel1};
+
+    phys::vec3 init_pos2{5,5,5};
+    phys::vec3 init_vel2{0,10,-10};
+    phys::Sphere *sphere2{nullptr};
+    sphere2 = new phys::Sphere{mass,init_pos2,init_vel2};
+
+    phys::vec3 gravity{0,-9.81,0};
+    float density{0};
+    phys::World world{gravity,density};
+    world.addSphere(sphere1);
+    world.addSphere(sphere2);
+    world.update();
+
+    phys::vec3 expected_pos1{0,0,0};
+    phys::vec3 expected_pos2{0,0,0};
+    expected_pos1 = init_pos1 + (init_vel1+gravity*world.getDt())*world.getDt();
+    expected_pos2 = init_pos2 + (init_vel2+gravity*world.getDt())*world.getDt();
 
     EXPECT_TRUE(vec3ExpectNear(expected_pos1,sphere1->getPosition(),.001f));
     EXPECT_TRUE(vec3ExpectNear(expected_pos2,sphere2->getPosition(),.001f));
